07c8628568081a2d75389eb219904311268f49d6
Sudha Maurya <rajendramaurya46223@gmail.com>
2026-07-06 15:24:53 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int bestBuy = prices[0], maxProfit = 0;
+
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] > bestBuy) {
+                maxProfit = max(maxProfit, prices[i] - bestBuy);
+            }
+
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
