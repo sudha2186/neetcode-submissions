5edc4910b9b80ea39e6c519dd97f7c85ef540c74
Sudha Maurya <rajendramaurya46223@gmail.com>
2026-07-12 01:08:14 +0530

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.cpp
@@ -0,0 +1,34 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+    }
+
+    void push(int x) {
+
+        q.push(x);
+
+        int n = q.size();
+
+        while (n > 1) {
+            q.push(q.front());
+            q.pop();
+            n--;
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
