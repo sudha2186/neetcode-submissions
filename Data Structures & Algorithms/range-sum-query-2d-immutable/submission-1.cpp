82f9efe2eaac05c9b5be41ca5b99b6c21fd96ebb
Sudha Maurya <rajendramaurya46223@gmail.com>
2026-08-29 15:29:12 +0530

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.cpp
@@ -0,0 +1,27 @@
+class NumMatrix {
+public:
+    vector<vector<int>> pre;
+
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        pre.resize(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                pre[i][j] = matrix[i - 1][j - 1]
+                          + pre[i - 1][j]
+                          + pre[i][j - 1]
+                          - pre[i - 1][j - 1];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return pre[row2 + 1][col2 + 1]
+             - pre[row1][col2 + 1]
+             - pre[row2 + 1][col1]
+             + pre[row1][col1];
+    }
+};
